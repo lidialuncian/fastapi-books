7748a95e95cad1b4b628c38da82b081476891c63
lidialuncian <lidia.luncian15@gmail.com>
2025-04-16 23:03:59 +0300

    database postgresql

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,12 +2,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///todosapp.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:root@localhost:5433/todoapp"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///todosapp.db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
 
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,6 +1,23 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
-router = APIRouter()
+from models import Users
+from passlib.context import CryptContext
+from typing import Annotated
+from fastapi import status
+from database import SessionLocal
+from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from jose import jwt, JWTError
+from datetime import timedelta, datetime, timezone
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["auth"]
+)
+SECRET_KEY = "0f1942cf8cf0c710c437b2702feddbc1e6be50889c57d73cf3cb1ac2cc9d80f9"
+ALGORITHM = "HS256"
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token")
 
 
 class CreateUserRequest(BaseModel):
@@ -11,8 +28,64 @@ class CreateUserRequest(BaseModel):
     password: str
     role: str
 
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@router.post("/auth")
-async def create_user(user: CreateUserRequest):
-    return {"user": "authenticated"}
+db_dependency = Annotated[Session, Depends(get_db)]
+
+def authenticate_user(username: str, password: str, db: db_dependency):
+    user_model = db.query(Users).filter(Users.username == username).first()
+    if user_model is None:
+        return False
+    if not bcrypt_context.verify(password, user_model.hashed_password):
+        return False
+    return user_model
+
+def create_access_token(username: str, user_id: int, role: str, expires_delta: timedelta):
+    encode = {"sub": username, "id": user_id, "role": role}
+    expire = datetime.now(timezone.utc) + expires_delta
+    encode.update({"exp": expire})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        user_id: int = payload.get("id")
+        role: str = payload.get("role")
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials")
+        return {"username": username, "id": user_id, "role": role}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials")
+    
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, user: CreateUserRequest):
+    create_user_model = Users(
+        username=user.username,
+        email=user.email,
+        firstname=user.firstname,
+        lastname=user.lastname,
+        hashed_password=bcrypt_context.hash(user.password),
+        role=user.role,
+        is_active=True,
+    )
+    db.add(create_user_model)
+    db.commit()
+    
+@router.post("/token", response_model=Token)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    user_model = authenticate_user(form_data.username, form_data.password, db)
+    if not user_model:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid username or password")
+    token = create_access_token(user_model.username, user_model.id, user_model.role, timedelta(minutes=20))
+    return Token(access_token=token, token_type="Bearer")
